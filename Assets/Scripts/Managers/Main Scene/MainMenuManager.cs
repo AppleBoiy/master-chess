6e9b4f91c6bedac11c0b9ba5a9bdb1eb33b6dc59
Meaww2 <rachata_tha@cmu.ac.th>
2023-02-25 22:08:23 +0700

    submit

--- Assets/Scripts/Managers/Main Scene/MainMenuManager.cs
+++ Assets/Scripts/Managers/Main Scene/MainMenuManager.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MainMenuManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class MainMenuManager : MonoBehaviour {
+    
+    public void playGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
