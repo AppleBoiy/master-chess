1608abdfa899c89719f666bb319f22afef1e6c29
ICCIE <contact.chaipat@gmail.com>
2023-02-26 19:13:52 +0700

    Fix merge Main menu and Chess scene conflicts

--- Assets/Scripts/Scriptables/MainMenuSceneManager.cs
+++ Assets/Scripts/Scriptables/MainMenuSceneManager.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
-public class MenuCotrol : MonoBehaviour
+public class MainMenuSceneManager : MonoBehaviour
 {   
     [Header("Volume Setting")]
-    [SerializeField] private TMP_Text volumeScale = null;
-    [SerializeField] private Slider volumeSlider = null;
-
+    [SerializeField] private TMP_Text volumeScale;
+    [SerializeField] private Slider volumeSlider;
+    
     [Header("Confirmation")]
-    [SerializeField] private GameObject comfirmationPrompt = null;
+    [SerializeField] private GameObject confirmationPrompt;
 
     private bool _isFullScreen;
     public void StartGameSinglePlayer()
@@ -20,31 +21,31 @@ public class MenuCotrol : MonoBehaviour
         SceneManager.LoadScene("Chess Scene");
     }
 
-    public void setVolume(float volume)
+    public void SetVolume(float volume)
     {
         AudioListener.volume = volume;
         volumeScale.text = volume.ToString("0.0");
     }
 
-    public void volumeApply()
+    public void VolumeApply()
     {
         PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
         StartCoroutine(ConfirmationBox());
     }
 
-    public IEnumerator ConfirmationBox()
+    private IEnumerator ConfirmationBox()
     {
-        comfirmationPrompt.SetActive(true);
+        confirmationPrompt.SetActive(true);
         yield return new WaitForSeconds(2);
-        comfirmationPrompt.SetActive(false);
+        confirmationPrompt.SetActive(false);
     }
 
-    public void setFullScreen(bool isFullScreen)
+    public void SetFullScreen(bool isFullScreen)
     {
         _isFullScreen = isFullScreen;
     }
 
-    public void fullScreenApply()
+    public void FullScreenApply()
     {
         PlayerPrefs.SetInt("masterFullScreen", (_isFullScreen ? 1 : 0));
         Screen.fullScreen = _isFullScreen;
