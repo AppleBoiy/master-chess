55200c327af7452cfff6431e414f6b6ff56fb396
ICCIE <contact.chaipat@gmail.com>
2023-03-02 14:37:42 +0700

    Add in-game setting

--- Assets/Scripts/Scriptables/Cursor/Promotion pawn scene/ScriptableSelectPromotion.cs
+++ Assets/Scripts/Scriptables/Cursor/Promotion pawn scene/ScriptableSelectPromotion.cs
@@ -1,7 +1,18 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public abstract class ScriptableSelectPromotion : MonoBehaviour, IPointerEnterHandler
+public abstract class ScriptableSelectPromotion : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler
 {
+    /// <summary>
+    /// When mouse enter on choose piece btn show information if it.
+    /// </summary>
+    /// <param name="eventData"></param>
     public abstract void OnPointerEnter(PointerEventData eventData);
+
+    
+    /// <summary>
+    /// When choose roll to promoted piece
+    /// </summary>
+    /// <param name="eventData"></param>
+    public abstract void OnPointerDown(PointerEventData eventData);
 }
