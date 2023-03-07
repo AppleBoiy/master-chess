ad94ae1b3acaccb4074387ed42c0cac1b55bb191
ICCIE <contact.chaipat@gmail.com>
2023-03-07 22:46:17 +0700

    add documentaries

--- Var/Template/Systems/Systems.cs
+++
@@ -1,8 +0,0 @@
-/// <summary>
-/// Don't specifically need anything here other than the fact it's persistent.
-/// I like to keep one main object which is never killed, with sub-systems as children.
-/// </summary>
-public class Systems : PersistentSingleton<Systems>
-{
-    
-}
